d25d2f4f0e82b7a0ebda742cfa8fd61de71dd9da
zaboltar <cletothar@gmail.com>
2019-04-09 00:34:29 -0500

    roboto thrusters

--- Udemy3D Part3/New Unity Project/Assets/Scripts/Rocket.cs
+++ Udemy3D Part3/New Unity Project/Assets/Scripts/Rocket.cs
@@ -5,35 +5,58 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    private Rigidbody rb;
+    Rigidbody rb;
+    AudioSource thrust;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        thrust = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     
        
     }
 
-    private void ProcessInput()
+    void Thrust()
     {
         if (Input.GetKey(KeyCode.Space)) //can thrust while rotating
         {
-            rb.AddRelativeForce(Vector3.up);
+            rb.AddRelativeForce(Vector3.up * mainThrust);
+            if (!thrust.isPlaying) // so it doesnt repeat >.<
+            {
+                thrust.Play();
+            }
+
         }
+        else
+        {
+            thrust.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rb.freezeRotation = true; // take manual control of rotation
+    
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
-        } else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+        rb.freezeRotation = false;    // resume physics control of rotation
     }
 }
