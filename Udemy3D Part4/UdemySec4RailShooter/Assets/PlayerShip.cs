728cf03af4bd1e7894c67e213dfafc7120985a23
zaboltar <cletothar@gmail.com>
2019-04-11 17:05:55 -0500

    circuit and camera flying and shoots wahoo

---
+++ Udemy3D Part4/UdemySec4RailShooter/Assets/PlayerShip.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerShip : MonoBehaviour
+{
+    [Tooltip("In mts per sec")] [SerializeField] float speed = 10f;
+    [Tooltip("In mts per sec")] [SerializeField] float xRange = 5f;
+    [Tooltip("In mts per sec")] [SerializeField] float yRange = 3f;
+
+    float xThrow, yThrow;
+
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField] float positionYawFactor = 5f;
+    [SerializeField] float controlRawFactor = -20f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+       
+    }
+
+    void ProcessTranslation()
+    {
+        xThrow = /*en lugar de input*/ CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * speed * Time.deltaTime;
+
+        float xRawPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(xRawPos, -xRange, xRange);
+
+
+
+        yThrow = /*en lugar de input*/ CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * speed * Time.deltaTime;
+
+        float yRawPos = transform.localPosition.y + yOffset;
+        float clampedyPos = Mathf.Clamp(yRawPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedyPos, transform.localPosition.z);
+
+    }
+
+    void ProcessRotation()
+    {
+        float pitchDueToPos = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToCon = yThrow * controlPitchFactor;
+
+        float pitch = pitchDueToPos + pitchDueToCon;
+
+        float yaw = transform.localPosition.x * positionYawFactor; 
+
+        float roll = xThrow * controlRawFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+}
---
+++ Udemy3D Part4/UdemySec4RailShooter/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake ()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
